32078e30774f9e6d3442cc5cf670e83430d28232
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2026-09-13 17:50:36 -0400

    started IR node to z3 translation method

--- src/z3_interface/Z3Interface.go
+++ src/z3_interface/Z3Interface.go
@@ -2,46 +2,78 @@ package z3_interface
 
 import (
 	"fmt"
+	"main/bmc"
 	"main/bmc/nodes"
 	"strconv"
 
 	z3 "github.com/Z3Prover/z3/src/api/go"
 )
 
-func Test() {
+func StateMachine(k int, graph nodes.DesignGraph) {
 
 	ctx := z3.NewContext()
+
 	solver := ctx.NewSolver()
 
-	x := ctx.MkBV(5, 32)
-	y := ctx.MkBV(5, 32)
+	frameDict := CreateFrameVars(ctx, &graph, k)
 
-	eq := ctx.MkEq(x, y)
+	for i := 0; i < k; i++ {
 
-	solver.Assert(eq)
+		for _, process := range graph.ProcessIRs {
+
+			for _, assign := range process.Assignments { // maybe I need assertion IOR
+
+				currentState := frameDict[assign.Target.Name][i]
+				target := frameDict[assign.Target.Name][i+1]
+				val := ExprToZ3(assign.Value, i, frameDict, ctx)
+				guard := ExprToZ3(assign.Guard, i, frameDict, ctx)
+
+				eq := ctx.MkEq(target, val) //for my implication -> guards anded and value equals target
+				falseEq := ctx.MkEq(target, currentState)
+
+				if guard != nil {
+
+					trueVal := ctx.MkBV(1, 1)
+					boolGuard := ctx.MkEq(guard, trueVal)
+
+					implication := ctx.MkImplies(boolGuard, eq)
+					falseImplication := ctx.MkImplies(ctx.MkNot(boolGuard), falseEq)
+
+					solver.Assert(implication)
+					solver.Assert(falseImplication)
+
+					print(implication.String())
+
+				} else {
+					combEq := ctx.MkEq(target, val)
+					solver.Assert(combEq)
+				}
+
+				_ = val
+				_ = guard
+
+			}
+
+		}
+
+	}
 
 	if solver.Check() == z3.Satisfiable {
-		fmt.Println("Satisfiable.")
-	} else if solver.Check() == z3.Unsatisfiable {
-		fmt.Println("Unsatisfiable")
+
+		if solver.Model() != nil {
+			fmt.Print(solver.Model().String())
+		}
+
+		print("Satisfiable!")
 	}
+	if solver.Check() == z3.Unsatisfiable {
 
-}
+		if solver.Model() != nil {
+			fmt.Print(solver.Model().String())
+		}
 
-func Test2(graph *nodes.DesignGraph) { //might need to edit graph ill leave pointer for now
-
-	ctx := z3.NewContext()
-	solver := ctx.NewSolver()
-	bitVectors := []*z3.Expr{}
-
-	for _, o := range graph.AssertionIRs {
-
-		temp := ctx.MkBV(o.Antecedent.Left.Width, uint(o.Antecedent.Left.Width))
-		bitVectors = append(bitVectors, temp)
-
+		print("Unsatisfiable!")
 	}
-
-	solver.Check()
 
 }
 
@@ -78,4 +110,89 @@ func CreateFrameVars(ctx *z3.Context, graph *nodes.DesignGraph, k int) map[strin
 	}
 
 	return exprMap
+}
+
+func ExprToZ3(expr *nodes.ExprIR, frame int, frameVars map[string][]*z3.Expr, ctx *z3.Context) *z3.Expr {
+
+	if expr == nil || expr.Kind == "" {
+		return nil
+	}
+
+	switch expr.Kind {
+
+	case "NamedValue":
+
+		if _, ok := frameVars[expr.Symbol]; ok {
+
+			return frameVars[expr.Symbol][frame]
+
+		} else {
+
+			return nil
+
+		}
+
+	case "IntegerLiteral":
+		val := bmc.ParseLiteralValue(expr.Value)
+		return ctx.MkBV(val, uint(expr.Width))
+
+	case "Conversion":
+		val := bmc.ParseLiteralValue(expr.Left.Value)
+		return ctx.MkBV(val, uint(expr.Width))
+
+	case "BinaryOp":
+		if expr.Op != "" {
+
+			if expr.Op == "LogicalAnd" {
+
+				left := ExprToZ3(expr.Left, frame, frameVars, ctx)
+				right := ExprToZ3(expr.Right, frame, frameVars, ctx)
+
+				implication := ctx.MkBVAnd(left, right)
+
+				fmt.Print(left.String() + "\n")
+				fmt.Print(right.String() + "\n")
+				fmt.Print(implication.String() + "\n")
+
+				return implication
+
+			}
+
+		}
+	case "ConditionalOp":
+
+		iteIf := ExprToZ3(expr.Args[0], frame, frameVars, ctx)
+		iteThen := ExprToZ3(expr.Args[1], frame, frameVars, ctx)
+		iteElse := ExprToZ3(expr.Args[2], frame, frameVars, ctx)
+
+		trueBv := ctx.MkBV(1, 1)
+		falseBv := ctx.MkBV(0, 1)
+
+		z3If := ctx.MkEq(iteIf, trueBv)
+		z3False := ctx.MkEq(iteIf, falseBv)
+
+		z3ThenHandle := ctx.MkImplies(z3If, iteThen)
+		z3FalseHandle := ctx.MkImplies(z3False, iteElse)
+
+		_ = z3ThenHandle
+		_ = z3FalseHandle
+
+	case "UnaryOp":
+
+		if expr.Op != "" {
+
+			switch expr.Op {
+
+			case "LogicalNot":
+				left := ExprToZ3(expr.Left, frame, frameVars, ctx)
+				return ctx.MkBVNot(left)
+
+			}
+
+		}
+
+	}
+
+	return nil
+
 }
