5d23c33745d95307fb17d8ae5f0f5e21f54e5c4f
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2026-09-13 12:40:27 -0400

    Added frame variable creation for bmc k unrolling

--- src/z3_interface/Z3Interface.go
+++ src/z3_interface/Z3Interface.go
@@ -2,6 +2,8 @@ package z3_interface
 
 import (
 	"fmt"
+	"main/bmc/nodes"
+	"strconv"
 
 	z3 "github.com/Z3Prover/z3/src/api/go"
 )
@@ -11,8 +13,8 @@ func Test() {
 	ctx := z3.NewContext()
 	solver := ctx.NewSolver()
 
-	x := ctx.MkBV(1, 32)
-	y := ctx.MkBV(1, 32)
+	x := ctx.MkBV(5, 32)
+	y := ctx.MkBV(5, 32)
 
 	eq := ctx.MkEq(x, y)
 
@@ -24,4 +26,56 @@ func Test() {
 		fmt.Println("Unsatisfiable")
 	}
 
+}
+
+func Test2(graph *nodes.DesignGraph) { //might need to edit graph ill leave pointer for now
+
+	ctx := z3.NewContext()
+	solver := ctx.NewSolver()
+	bitVectors := []*z3.Expr{}
+
+	for _, o := range graph.AssertionIRs {
+
+		temp := ctx.MkBV(o.Antecedent.Left.Width, uint(o.Antecedent.Left.Width))
+		bitVectors = append(bitVectors, temp)
+
+	}
+
+	solver.Check()
+
+}
+
+func CreateFrameVars(ctx *z3.Context, graph *nodes.DesignGraph, k int) map[string][]*z3.Expr {
+
+	exprMap := make(map[string][]*z3.Expr)
+
+	add := func(name string, width int) {
+
+		if _, ok := exprMap[name]; ok {
+			return
+		}
+
+		for t := 0; t <= k; t++ {
+
+			sym := ctx.MkStringSymbol(name + "_" + strconv.Itoa(t))
+
+			v := ctx.MkConst(sym, ctx.MkBvSort(uint(width)))
+
+			exprMap[name] = append(exprMap[name], v)
+
+		}
+
+	}
+
+	for _, s := range graph.Inputs {
+		add(s.Name, s.Width)
+	}
+	for _, s := range graph.Signals {
+		add(s.Name, s.Width)
+	}
+	for _, s := range graph.Outputs {
+		add(s.Name, s.Width)
+	}
+
+	return exprMap
 }
